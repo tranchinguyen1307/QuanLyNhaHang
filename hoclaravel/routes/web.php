81b7c9632510b98d6f86b44783b89aded76e60a6
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-07-17 23:01:56 +0700

    HS1 - Cắt giao diện Client trang home

--- hoclaravel/routes/web.php
+++ hoclaravel/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', function () {
     return view('test');
 });
+Route::get('/trang-chu',[HomeController::class, 'index']);
