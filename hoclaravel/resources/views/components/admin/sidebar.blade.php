1bc84106368ab6a12f6687ea5046634ae08ae5b8
SuzzenD <nhatnmpc06310@fpt.edu.vn>
2024-07-22 03:38:01 +0700

    cat-giao-dien-employees-admin

--- hoclaravel/resources/views/components/admin/sidebar.blade.php
+++ hoclaravel/resources/views/components/admin/sidebar.blade.php
@@ -116,6 +116,29 @@
                         </li>
                     </ul>
                 </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-bars"></i>
+                        <p>
+                            Nhân viên
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('employees') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Danh sách</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('employees.create') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Thêm</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
