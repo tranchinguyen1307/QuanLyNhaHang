5da5ca8b2278e3f9f72df6dcd2af96df0b4f94b1
toankhung <lyminhtoan2004@gmail.com>
2024-07-21 15:24:24 +0700

    cat-giao-dien-menu-admin
    cắt giao diện menu admin

--- hoclaravel/routes/web.php
+++ hoclaravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\MenuController as AdminHomeController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,3 +10,6 @@ Route::get('/test', function () {
     return view('test');
 });
 Route::get('/trang-chu',[HomeController::class, 'index']);
+Route::get('/menu', [AdminHomeController::class, 'index'])->name('menu');
+Route::get('/menu/create', [AdminHomeController::class, 'create'])->name('menu.create');
+Route::get('/menu/edit', [AdminHomeController::class, 'edit'])->name('menu.edit');
