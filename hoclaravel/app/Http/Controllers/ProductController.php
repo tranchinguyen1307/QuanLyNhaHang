da4933068c37c03eb28034a69110c06371c901a5
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-07-24 23:04:12 +0700

    Hiển thị danh mục và hiển thị sản phẩm theo danh mục

--- hoclaravel/app/Http/Controllers/ProductController.php
+++ hoclaravel/app/Http/Controllers/ProductController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Support\Collection;
+
 class ProductController extends Controller
 {
-    public function index(){
-        $products = Product::all();
-        return view('client.menu', compact('products'));
+    public function index()
+    {
+        $categories = $this->categories();
+        $products = $this->products($categories);
+
+        return view('client.menu', compact('products', 'categories'));
+    }
+
+    public function products(Collection $categories): array
+    {
+        $productsByCategory = [];
+
+        foreach ($categories as $category) {
+
+            $productsByCategory[$category->id] = Product::where('category_id', $category->id)->get();
+        }
+
+        return $productsByCategory;
+    }
+
+    public function categories(): Collection
+    {
+        return Category::all();
     }
 }
