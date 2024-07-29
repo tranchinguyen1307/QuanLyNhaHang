830c76a7fb5c9c5cd6f2e4a7d9512f1263ffd5a1
SuzzenD <nhatnmpc06310@fpt.edu.vn>
2024-07-29 14:44:22 +0700

    xử lý xung đột

--- hoclaravel/resources/views/navigation-menu.blade.php
+++ hoclaravel/resources/views/navigation-menu.blade.php
@@ -5,33 +5,21 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-<<<<<<< HEAD
-<<<<<<< HEAD
                     <a href="{{ route('/home') }}">
-=======
                     <a href="{{ route('dashboard') }}">
->>>>>>> origin/minhToan
-=======
                     <a href="{{ route('/home') }}">
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-<<<<<<< HEAD
-<<<<<<< HEAD
                     <x-nav-link href="{{ route('/home') }}" :active="request()->routeIs('/home')">
                         {{ __('/home') }}
-=======
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
->>>>>>> origin/minhToan
-=======
                     <x-nav-link href="{{ route('/home') }}" :active="request()->routeIs('/home')">
                         {{ __('/home') }}
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                     </x-nav-link>
                 </div>
             </div>
@@ -43,10 +31,6 @@
                         <x-dropdown align="right" width="60">
                             <x-slot name="trigger">
                                 <span class="inline-flex rounded-md">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                                     <button type="button"
                                         class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->currentTeam->name }}
@@ -55,16 +39,11 @@
                                             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-<<<<<<< HEAD
-=======
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->currentTeam->name }}
 
                                         <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
->>>>>>> origin/minhToan
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                                         </svg>
                                     </button>
                                 </span>
@@ -111,10 +90,6 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                                 <button
                                     class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                     <img class="h-8 w-8 rounded-full object-cover"
@@ -130,8 +105,6 @@
                                             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-<<<<<<< HEAD
-=======
                                 <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                     <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                 </button>
@@ -142,9 +115,6 @@
 
                                         <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
->>>>>>> origin/minhToan
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                                         </svg>
                                     </button>
                                 </span>
@@ -172,17 +142,10 @@
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" x-data>
                                 @csrf
-
-<<<<<<< HEAD
-<<<<<<< HEAD
                                 <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-=======
                                 <x-dropdown-link href="{{ route('logout') }}"
                                          @click.prevent="$root.submit();">
->>>>>>> origin/minhToan
-=======
                                 <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
@@ -193,10 +156,6 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                 <button @click="open = ! open"
                     class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -205,15 +164,10 @@
                             d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
                             stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-<<<<<<< HEAD
-=======
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
->>>>>>> origin/minhToan
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                     </svg>
                 </button>
             </div>
@@ -221,23 +175,14 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('/home') }}" :active="request()->routeIs('/home')">
                 {{ __('/home') }}
-<<<<<<< HEAD
-=======
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
->>>>>>> origin/minhToan
-=======
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
             </x-responsive-nav-link>
         </div>
 
@@ -246,17 +191,11 @@
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="shrink-0 me-3">
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
                             alt="{{ Auth::user()->name }}" />
-=======
                         <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
->>>>>>> origin/minhToan
-=======
                         <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
                             alt="{{ Auth::user()->name }}" />
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                     </div>
                 @endif
 
@@ -282,16 +221,10 @@
                 <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
 
-<<<<<<< HEAD
-<<<<<<< HEAD
                     <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-=======
                     <x-responsive-nav-link href="{{ route('logout') }}"
                                    @click.prevent="$root.submit();">
->>>>>>> origin/minhToan
-=======
                     <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
@@ -305,17 +238,11 @@
                     </div>
 
                     <!-- Team Settings -->
-<<<<<<< HEAD
-<<<<<<< HEAD
                     <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}"
                         :active="request()->routeIs('teams.show')">
-=======
                     <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
->>>>>>> origin/minhToan
-=======
                     <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}"
                         :active="request()->routeIs('teams.show')">
->>>>>>> 39cfba0bfea50a4ea553bd50de55c6de9645135b
                         {{ __('Team Settings') }}
                     </x-responsive-nav-link>
 
