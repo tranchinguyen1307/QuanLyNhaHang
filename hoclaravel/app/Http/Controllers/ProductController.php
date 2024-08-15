65c1bdafc649fd83644f0be05196fe1c4d46fe54
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-08-15 21:15:01 +0700

    Product_Detail-Update
    (cherry picked from commit e1add4b69f7c0ba378fbf6b51076b63e874e8c7e)

--- hoclaravel/app/Http/Controllers/ProductController.php
+++ hoclaravel/app/Http/Controllers/ProductController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Support\Collection;
+use App\Models\Comment;
+use App\Models\User;
 
+use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index()
@@ -27,7 +30,33 @@ class ProductController extends Controller
 
         return $productsByCategory;
     }
+    public function product_detail($id) {
+        $product = Product::find($id);
+        $comments = Comment::where('product_id', $id)
+        ->with('user')
+        ->orderBy('created_at', 'desc') // Sắp xếp theo ngày tạo mới nhất
+        ->get();
+            return view('client.pages.detail', ['product' => $product,'comments'=>$comments]);
+    }
+    public function product_comment($id,Request $request) {
 
+
+        Comment::create([
+            'product_id' => $id,
+            'user_id' => auth()->user()->id,
+            'comment' => $request->input('comment'),
+
+        ]);
+        return redirect()->route('client.san-pham.detail',['id' => $id]);
+
+    }
+    public function product_comment_destroy($id,$id_product) {
+        $comment = Comment::find($id);
+
+        $comment->delete();
+        return redirect()->route('client.san-pham.detail',['id' => $id_product]);
+
+    }
     public function categories(): Collection
     {
         return Category::all();
