051fef961d349aefac746ace989454fc0e472da5
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-08-15 21:16:31 +0700

    Layout_Update
    (cherry picked from commit b273e2b81c990a744e3ed0468c0300e15b7f9873)

--- hoclaravel/resources/views/components/admin/footer.blade.php
+++ hoclaravel/resources/views/components/admin/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>Quản lý nhà hàng T8</a>.</strong>
     <div class="float-right d-none d-sm-inline-block">
+        <b>Laravel</b>
         <b>Version</b> 3.2.0-rc
     </div>
 </footer>
@@ -47,7 +47,7 @@
 <!-- AdminLTE -->
 <script src="{{ asset('assets/dist/js/adminlte.js') }}"></script>
 <script>
-    $(function() {
+    $(function () {
         // Summernote
         $('#summernote').summernote()
 
